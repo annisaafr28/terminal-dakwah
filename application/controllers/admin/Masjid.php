ed404209782c6fc0dbd25cf66d0aedc69cc14b5a
bintang4974 <bintangramadhan4974@gmail.com>
2022-02-17 13:09:33 +0700

    adding modul masjid & jadwal kajian

--- application/controllers/admin/Masjid.php
+++ application/controllers/admin/Masjid.php
@@ -2,37 +2,32 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Ustad extends CI_Controller
+class Masjid extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         $this->load->model('inventory_model');
-
-        if ($this->session->userdata('role') != 'admin') {
-            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">Anda belum login!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-            redirect('auth');
-        }
     }
 
     public function index()
     {
-        $data['title'] = 'Ustad';
-        $data['ustad'] = $this->inventory_model->get_data('ustad')->result();
+        $data['title'] = 'Masjid';
+        $data['masjid'] = $this->inventory_model->get_data('masjid')->result();
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
-        $this->load->view('admin/ustad/ustad', $data);
+        $this->load->view('admin/masjid/masjid', $data);
         $this->load->view('templates/footer');
     }
 
     public function tambah()
     {
-        $data['title'] = 'Tambah Ustad';
+        $data['title'] = 'Tambah Masjid';
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
-        $this->load->view('admin/ustad/tambah_ustad');
+        $this->load->view('admin/masjid/tambah_masjid');
         $this->load->view('templates/footer');
     }
 
@@ -59,26 +54,26 @@ class Ustad extends CI_Controller
             }
 
             $data = array(
-                'nama_ustad'    => $this->input->post('nama_ustad'),
-                'alamat_ustad'  => $this->input->post('alamat_ustad'),
-                'no_hp'         => $this->input->post('no_hp'),
+                'nama_masjid'   => $this->input->post('nama_masjid'),
+                'alamat'        => $this->input->post('alamat'),
+                'url_maps'      => $this->input->post('url_maps'),
                 'foto'          => $foto
             );
 
-            $this->inventory_model->insert_data($data, 'ustad');
+            $this->inventory_model->insert_data($data, 'masjid');
             $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">Data Berhasil Ditambahkan!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-            redirect('admin/ustad');
+            redirect('admin/masjid');
         }
     }
 
     public function edit_data($id)
     {
-        $data['title'] = 'Edit Ustad';
-        $data['ustad'] = $this->db->query("SELECT * FROM ustad WHERE id_ustad='$id'")->result();
+        $data['title'] = 'Edit Masjid';
+        $data['masjid'] = $this->db->query("SELECT * FROM masjid WHERE id_masjid='$id'")->result();
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
-        $this->load->view('admin/ustad/edit_ustad', $data);
+        $this->load->view('admin/masjid/edit_masjid', $data);
         $this->load->view('templates/footer');
     }
 
@@ -96,7 +91,7 @@ class Ustad extends CI_Controller
 
                 $this->load->library('upload', $config);
 
-                if($this->upload->do_upload('foto')){
+                if ($this->upload->do_upload('foto')) {
                     $foto = $this->upload->data('file_name');
                     $this->db->set('foto', $foto);
                 } else {
@@ -105,36 +100,36 @@ class Ustad extends CI_Controller
             }
 
             $data = array(
-                'nama_ustad'    => $this->input->post('nama_ustad'),
-                'alamat_ustad'  => $this->input->post('alamat_ustad'),
-                'no_hp'         => $this->input->post('no_hp'),
+                'nama_masjid'   => $this->input->post('nama_masjid'),
+                'alamat'        => $this->input->post('alamat'),
+                'url_maps'      => $this->input->post('url_maps'),
             );
 
             $where = array(
-                'id_ustad' => $this->input->post('id_ustad'),
+                'id_masjid' => $this->input->post('id_masjid'),
             );
 
-            $this->inventory_model->update_data('ustad', $data, $where);
+            $this->inventory_model->update_data('masjid', $data, $where);
             $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">Data Berhasil Diubah!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-            redirect('admin/ustad');
+            redirect('admin/masjid');
         }
     }
 
     public function _rules()
     {
-        $this->form_validation->set_rules('nama_ustad', 'Nama Ustad', 'required');
-        $this->form_validation->set_rules('alamat_ustad', 'Alamat Ustad', 'required');
-        $this->form_validation->set_rules('no_hp', 'No Telp', 'required');
+        $this->form_validation->set_rules('nama_masjid', 'Nama Masjid', 'required');
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required');
+        $this->form_validation->set_rules('url_maps', 'URL Maps', 'required');
     }
 
     public function delete_data($id)
     {
-        $where = array('id_ustad' => $id);
+        $where = array('id_masjid' => $id);
 
-        $this->inventory_model->delete_data($where, 'ustad');
+        $this->inventory_model->delete_data($where, 'masjid');
         $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">Data Berhasil Dihapus!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-        redirect('admin/ustad');
+        redirect('admin/masjid');
     }
 }
 
-/* End of file Barang.php */
+/* End of file Masjid.php */
---
+++ application/views/admin/jadwal_kajian/jadwal_kajian.php
@@ -0,0 +1,46 @@
+<?= $this->session->flashdata('pesan') ?>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <a href="<?= base_url('admin/masjid/tambah') ?>" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i> Tambah</a>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr class="text-center">
+                        <th>No</th>
+                        <th>Nama Masjid</th>
+                        <th>Nama Ustad</th>
+                        <th>Kajian</th>
+                        <th>Tanggal</th>
+                        <th>Waktu</th>
+                        <th>Keterangan</th>
+                        <th>Flyer Kajian</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $no = 1;
+                    foreach ($jadwal_kajian as $jk) : ?>
+                        <tr class="text-center">
+                            <td><?= $no++ ?></td>
+                            <td><?= $jk->nama_masjid ?></td>
+                            <td><?= $jk->nama_ustad ?></td>
+                            <td><?= $jk->id_kajian ?></td>
+                            <td><?= $jk->tanggal ?></td>
+                            <td><?= $jk->waktu ?></td>
+                            <td><?= $jk->keterangan ?></td>
+                            <td><?= $jk->flyer_kajian ?></td>
+                            <td>
+                                <a href="<?= base_url('admin/jadwal_kajian/edit_data/' . $jk->id_jadwal_kajian) ?>" class="btn btn-circle btn-warning"><i class="fas fa-edit"></i></a>
+                                <a href="<?= base_url('admin/jadwal_kajian/delete_data/' . $jk->id_jadwal_kajian) ?>" class="btn btn-circle btn-danger" onclick="return confirm('Apakah anda yakin mengahpus data ini?')"><i class="fas fa-trash"></i></a>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
